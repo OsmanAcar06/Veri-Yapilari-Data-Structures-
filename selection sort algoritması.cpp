1c4ef129e7c619f2d640826d5124480e3aad6663
OsmanAcar06 <acarosman252@gmail.com>
2024-12-05 22:09:18 +0300

    selection sort algorithm.cpp

--- selection sort algoritması.cpp
+++ selection sort algoritması.cpp
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
 // Selection Sort Function
 void selection_Sort(int A[], int n) {
     int i, temp;
-
 for(int i=0; i<n-1; i++ ){
 	int minindis=i;  
 	printf("Baslangic minindis = %d  ", minindis);
@@ -22,18 +20,12 @@ for(int i=0; i<n-1; i++ ){
             printf("%d, ", A[k]);
         printf("\n");
     }	
-
 }
-
-
-
 int main() {
     int n;
-
     // Get array size from user
     printf("Kac elemanli bir dizi siralamak istiyorsunuz? ");
     scanf("%d", &n);
-
     int A[n];
 
     // Retrieve array elements from the user
@@ -42,21 +34,15 @@ int main() {
         printf("A[%d]: ", i);
         scanf("%d", &A[i]);
     }
-
     // array before sorting
     printf("Siralama oncesi dizi: ");
     for (int i = 0; i < n; i++)
         printf("%d, ", A[i]);
     printf("\n");
-
     selection_Sort(A, n);
-
     // array after sorting
     printf("Siralama sonrasi dizi: ");
     for (int i = 0; i < n; i++)
         printf("%d, ", A[i]);
     printf("\n");
-
-    return 0;
 }
-
