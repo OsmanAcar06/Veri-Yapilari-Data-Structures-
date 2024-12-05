2b49aa17eb775dfa196e2a9f60bf61dd0e30ee9d
OsmanAcar06 <acarosman252@gmail.com>
2024-12-05 22:14:18 +0300

    Add files via upload

---
+++ insertion sort algoritması.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+// Mevcut Insertion Sort fonksiyonu 
+void insertion_Sort(int A[], int n) {
+int i,j,k,temp;
+	
+	for(k=1; k<n; k++ ){
+	temp = A[k];
+	j=k-1; 
+	while((temp<A[j] )&& j>=0 ){
+		A[j+1] = A[j];
+		j=j-1; 
+	}
+	A[j+1]=temp;
+}
+for(k=0; k<n; k++){
+	printf("%d ", A[k]); // Sýralanmýþ diziyi gösterir. 
+}
+}
+int main() {
+    int n, i;
+
+    // Kullanýcýdan dizi boyutunu ve elemanlarýný alma
+    printf("Kac elemanli bir dizi siralamak istiyorsunuz? ");
+    scanf("%d", &n);
+
+    int A[n];
+    printf("Dizi elemanlarýný giriniz:\n");
+    for (i = 0; i < n; i++) {
+        printf("A[%d]: ", i);
+        scanf("%d", &A[i]);
+    }
+
+    // Sýralama öncesi dizi
+    printf("Siralama öncesi dizi: ");
+    for (i = 0; i < n; i++)
+        printf("%d, ", A[i]);
+    printf("\n");
+
+    // Insertion Sort ile sýralama
+    printf("Siralama islemi basliyor...\n");
+    insertion_Sort(A, n);
+    
+}
+
